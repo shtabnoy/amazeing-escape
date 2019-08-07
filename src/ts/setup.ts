ebf96e93db960f93fdf2cc234a1f8f30e62fc479
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2019-08-08 00:57:20 +0200

    refactor

--- src/ts/setup.ts
+++ src/ts/setup.ts
@@ -1,19 +1,6 @@
 import Graph from './Graph'
 import mst from './MST'
 
-interface Maze {
-  [key: string]: {
-    left: number | null
-    right: number | null
-    up: number | null
-    down: number | null
-    x1: number
-    y1: number
-    x2: number
-    y2: number
-  }
-}
-
 interface Point {
   x: number
   y: number
@@ -28,13 +15,24 @@ interface Walls {
   [key: string]: Wall
 }
 
-const walls: Walls = {}
+interface Keys {
+  [key: string]: boolean
+}
 
+const walls: Walls = {}
 const WIDTH = 5
 const HEIGHT = 5
 const CIRCLE_R = 5
+const STEP = 2
+const CW = 600 // window.innerWidth
+const CH = 600 // window.innerHeight
+const coords: Point = {
+  x: 30,
+  y: 30,
+}
+const keys: Keys = {}
 
-function drawOnCanvas(ctx: CanvasRenderingContext2D) {
+const drawWalls = (ctx: CanvasRenderingContext2D) => {
   Object.values(walls).forEach(wall => {
     ctx.beginPath()
     ctx.moveTo(wall.a.x, wall.a.y)
@@ -44,27 +42,16 @@ function drawOnCanvas(ctx: CanvasRenderingContext2D) {
   })
 }
 
-function drawCharacter(ctx: CanvasRenderingContext2D, x: number, y: number) {
+const drawCharacter = (ctx: CanvasRenderingContext2D) => {
   ctx.beginPath()
-  ctx.arc(x, y, CIRCLE_R, 0, 2 * Math.PI)
+  ctx.arc(coords.x, coords.y, CIRCLE_R, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.fill()
   ctx.closePath()
 }
 
-function prepareForDrawing(g: Graph) {
-  const mazeObject: Maze = {}
+const prepareForDrawing = (g: Graph) => {
   Object.keys(g.AdjList).forEach((v1, index) => {
-    mazeObject[v1] = {
-      left: null,
-      right: null,
-      up: null,
-      down: null,
-      x1: 0,
-      y1: 0,
-      x2: 0,
-      y2: 0,
-    }
     const rw = 50
     const offset = 10
     let topLeft = {
@@ -83,10 +70,6 @@ function prepareForDrawing(g: Graph) {
       x: ((index % WIDTH) + 1) * rw + offset,
       y: (Math.floor(index / HEIGHT) + 1) * rw + offset,
     }
-    mazeObject[v1].x1 = (index % WIDTH) * rw + offset
-    mazeObject[v1].x2 = ((index % WIDTH) + 1) * rw + offset
-    mazeObject[v1].y1 = Math.floor(index / HEIGHT) * rw + offset
-    mazeObject[v1].y2 = (Math.floor(index / HEIGHT) + 1) * rw + offset
 
     walls[`${topLeft.x}${topLeft.y}${bottomLeft.x}${bottomLeft.y}`] = {
       a: topLeft,
@@ -106,31 +89,26 @@ function prepareForDrawing(g: Graph) {
     }
     Object.keys(g.AdjList[v1]).forEach(v2 => {
       if (Number(v1) - Number(v2) === 1) {
-        mazeObject[v1].left = Number(v2)
         delete walls[`${topLeft.x}${topLeft.y}${bottomLeft.x}${bottomLeft.y}`]
       }
       if (Number(v1) - Number(v2) === WIDTH) {
-        mazeObject[v1].up = Number(v2)
         delete walls[`${topLeft.x}${topLeft.y}${topRight.x}${topRight.y}`]
       }
       if (Number(v1) - Number(v2) === -1) {
-        mazeObject[v1].right = Number(v2)
         delete walls[
           `${topRight.x}${topRight.y}${bottomRight.x}${bottomRight.y}`
         ]
       }
       if (Number(v1) - Number(v2) === -WIDTH) {
-        mazeObject[v1].down = Number(v2)
         delete walls[
           `${bottomLeft.x}${bottomLeft.y}${bottomRight.x}${bottomRight.y}`
         ]
       }
     })
   })
-  return mazeObject
 }
 
-function makeGraph(w: number, h: number) {
+const makeGraph = (w: number, h: number) => {
   const g1 = new Graph()
   const g2 = new Graph()
   Object.keys(Array.from(Array(w * h))).forEach((n, index) => {
@@ -180,137 +158,118 @@ function makeGraph(w: number, h: number) {
   return g2
 }
 
-const rerender = (
-  ctx: CanvasRenderingContext2D,
-  maze: Maze,
-  cw: number,
-  ch: number,
-  coords: Point
-) => {
-  ctx.clearRect(0, 0, cw, ch)
-  drawOnCanvas(ctx)
-  drawCharacter(ctx, coords.x, coords.y)
+const rerender = (ctx: CanvasRenderingContext2D) => {
+  ctx.clearRect(0, 0, CW, CH)
+  drawWalls(ctx)
+  drawCharacter(ctx)
 }
+
+const move = (
+  ctx: CanvasRenderingContext2D,
+  dir: 'left' | 'right' | 'up' | 'down'
+) => {
+  let collided = false
+  switch (dir) {
+    case 'left':
+      Object.values(walls).forEach(wall => {
+        if (
+          coords.x - STEP >= wall.b.x &&
+          coords.x - STEP - CIRCLE_R <= wall.b.x &&
+          coords.y + CIRCLE_R >= wall.a.y &&
+          coords.y - CIRCLE_R <= wall.b.y
+        ) {
+          collided = true
+        }
+      })
+      if (!collided) coords.x -= STEP
+      break
+    case 'right':
+      Object.values(walls).forEach(wall => {
+        if (
+          coords.x + STEP <= wall.a.x &&
+          coords.x + STEP + CIRCLE_R >= wall.a.x &&
+          coords.y + CIRCLE_R >= wall.a.y &&
+          coords.y - CIRCLE_R <= wall.b.y
+        ) {
+          collided = true
+        }
+      })
+      if (!collided) coords.x += STEP
+      break
+    case 'up':
+      Object.values(walls).forEach(wall => {
+        if (
+          coords.y - STEP >= wall.b.y &&
+          coords.y - STEP - CIRCLE_R <= wall.b.y &&
+          coords.x + CIRCLE_R >= wall.a.x &&
+          coords.x - CIRCLE_R <= wall.b.x
+        ) {
+          collided = true
+        }
+      })
+      if (!collided) coords.y -= STEP
+      break
+    case 'down':
+      Object.values(walls).forEach(wall => {
+        if (
+          coords.y + STEP <= wall.a.y &&
+          coords.y + STEP + CIRCLE_R >= wall.a.y &&
+          coords.x + CIRCLE_R >= wall.a.x &&
+          coords.x - CIRCLE_R <= wall.b.x
+        ) {
+          collided = true
+        }
+      })
+      if (!collided) coords.y += STEP
+      break
+    default:
+      break
+  }
+  rerender(ctx)
+  collided = false
+}
+
+const update = (ctx: CanvasRenderingContext2D) => {
+  if (keys[39]) {
+    move(ctx, 'right')
+  }
+  if (keys[37]) {
+    move(ctx, 'left')
+  }
+  if (keys[40]) {
+    move(ctx, 'down')
+  }
+  if (keys[38]) {
+    move(ctx, 'up')
+  }
+  requestAnimationFrame(() => update(ctx))
+}
+
+document.addEventListener('keydown', e => {
+  keys[e.keyCode] = true
+})
+document.addEventListener('keyup', e => {
+  keys[e.keyCode] = false
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const graph = makeGraph(WIDTH, HEIGHT)
-  const maze = prepareForDrawing(graph)
-
   const canvas = document.getElementById('canvas') as HTMLCanvasElement
-  // const cw = window.innerWidth
-  // const ch = window.innerHeight
-  const cw = 600
-  const ch = 600
-
-  // retina adjustments
-  canvas.width = cw * 2
-  canvas.height = ch * 2
-  canvas.style.width = `${cw}px`
-  canvas.style.height = `${ch}px`
   const ctx = canvas.getContext('2d')
   const ratio = window.devicePixelRatio
-  ctx.scale(ratio, ratio)
 
+  // retina adjustments
+  canvas.width = CW * 2
+  canvas.height = CH * 2
+  canvas.style.width = `${CW}px`
+  canvas.style.height = `${CH}px`
   ctx.strokeStyle = 'black'
   ctx.lineWidth = 2
   ctx.lineCap = 'square'
+  ctx.scale(ratio, ratio)
 
-  const coords = {
-    x: 30,
-    y: 30,
-  }
-  const STEP = 2
-  drawOnCanvas(ctx)
-  drawCharacter(ctx, coords.x, coords.y)
-
-  const move = (dir: 'left' | 'right' | 'up' | 'down') => {
-    let collided = false
-    switch (dir) {
-      case 'left':
-        Object.values(walls).forEach(wall => {
-          if (
-            coords.x - STEP >= wall.b.x &&
-            coords.x - STEP - CIRCLE_R <= wall.b.x &&
-            coords.y + CIRCLE_R >= wall.a.y &&
-            coords.y - CIRCLE_R <= wall.b.y
-          ) {
-            collided = true
-          }
-        })
-        if (!collided) coords.x -= STEP
-        break
-      case 'right':
-        Object.values(walls).forEach(wall => {
-          if (
-            coords.x + STEP <= wall.a.x &&
-            coords.x + STEP + CIRCLE_R >= wall.a.x &&
-            coords.y + CIRCLE_R >= wall.a.y &&
-            coords.y - CIRCLE_R <= wall.b.y
-          ) {
-            collided = true
-          }
-        })
-        if (!collided) coords.x += STEP
-        break
-      case 'up':
-        Object.values(walls).forEach(wall => {
-          if (
-            coords.y - STEP >= wall.b.y &&
-            coords.y - STEP - CIRCLE_R <= wall.b.y &&
-            coords.x + CIRCLE_R >= wall.a.x &&
-            coords.x - CIRCLE_R <= wall.b.x
-          ) {
-            collided = true
-          }
-        })
-        if (!collided) coords.y -= STEP
-        break
-      case 'down':
-        Object.values(walls).forEach(wall => {
-          if (
-            coords.y + STEP <= wall.a.y &&
-            coords.y + STEP + CIRCLE_R >= wall.a.y &&
-            coords.x + CIRCLE_R >= wall.a.x &&
-            coords.x - CIRCLE_R <= wall.b.x
-          ) {
-            collided = true
-          }
-        })
-        if (!collided) coords.y += STEP
-        break
-      default:
-        break
-    }
-    rerender(ctx, maze, cw, ch, coords)
-    collided = false
-  }
-
-  const keys: {
-    [key: string]: boolean
-  } = {}
-
-  const update = () => {
-    if (keys[39]) {
-      move('right')
-    }
-    if (keys[37]) {
-      move('left')
-    }
-    if (keys[40]) {
-      move('down')
-    }
-    if (keys[38]) {
-      move('up')
-    }
-    requestAnimationFrame(update)
-  }
-
-  requestAnimationFrame(update)
-
-  document.addEventListener('keydown', e => {
-    keys[e.keyCode] = true
-  })
-  document.addEventListener('keyup', e => {
-    keys[e.keyCode] = false
-  })
+  prepareForDrawing(graph)
+  drawWalls(ctx)
+  drawCharacter(ctx)
+  requestAnimationFrame(() => update(ctx))
 })
